9fb5d5806d9049aea430e8b0006be3beaea769ad
Yi Fan Song <yifan.song@123loadboard.com>
2021-09-18 07:18:52 -0400

    Stop player from moving off screen

---
+++ UbiGame_Building/Source/GameEngine/Util/ConfigurationManager.cpp
@@ -0,0 +1,22 @@
+#include "ConfigurationManager.h"
+
+using namespace GameEngine;
+
+ConfigurationManager* ConfigurationManager::sm_instance = nullptr;
+	
+ConfigurationManager::ConfigurationManager()
+	: m_windowSize(nullptr)
+{
+	m_windowSize = new sf::Vector2f(500, 500);
+}
+
+ConfigurationManager::~ConfigurationManager()
+{
+
+}
+
+void ConfigurationManager::ResizeWindow(sf::Vector2f* newSize)
+{
+	m_windowSize = newSize;
+}
+
